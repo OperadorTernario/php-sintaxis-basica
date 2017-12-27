87377c569ade13e4a2eafdf5ee1eff8752d8ea97
Darío Rivera <fermius.us@gmail.com>
2017-12-27 01:59:21 -0500

    Primer commit, video subido!

---
+++ README.md
@@ -0,0 +1,5 @@
+## Sintaxis básica en PHP - Escape, sentencias, comentarios
+
+Este es un ejemplo básica de escape de código en PHP, sentencias y comentarios.
+
+Vista [Curso PHP Básico #2 - Escape de código, sentecias y comentarios](https://youtu.be/FBn4kFZJ_RM) para ver el video!
---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Sintaxis Básica en PHP</title>
+</head>
+<body>
+	<?php
+		echo "Hola mundo! <br />";		// escape clásico
+	?>
+
+	<?= "Hola mundo! <br />" ?>		<!-- Short echo tag -->
+
+	<?php
+		echo "Hello world! <br />";  // sentencia una línea
+		echo 
+			"Hello world 2! <br />";  // sentencia en dos líneas
+	?>
+
+	<?php
+		// estilo C++
+		/* comentario
+				multilínea */
+		# estilo shell
+	?>
+</body>
+</html>
